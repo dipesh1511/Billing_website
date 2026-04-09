e48af9edcaeeafc3cde3fefab6a3ff747d9b8b8b
Dipesh Dodke <dipeshdodke15@gmail.com>
2026-04-09 13:12:56 +0530

    adding server file

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected {(local)}"))
+.catch(err => console.log(err));
+
+
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/dashboard", require("./routes/dashboardRoutes"));
+
+
+
+const PORT = process.env.PORT || 5000;
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
